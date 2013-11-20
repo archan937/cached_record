912cc4f4e2e80d3068db763e21ce8072a97de1f9
Paul Engel <paul.engel@holder.nl>
2013-11-20 01:31:26 +0100

    Logging ActiveRecord and DataMapper

--- script/setup.rb
+++ script/setup.rb
@@ -1,9 +1,13 @@
 require "yaml"
+require "logger"
 
 dbconfig = YAML.load_file(File.expand_path("../../config/database.yml", __FILE__))["development"]
+logger = Logger.new("log/development.log")
+
 ActiveRecord::Base.establish_connection dbconfig
 ActiveRecord::Base.time_zone_aware_attributes = true
 ActiveRecord::Base.default_timezone = :local
+ActiveRecord::Base.logger = logger
 
 CachedRecord.setup :redis
 Redis.new.flushdb
--- test/test_helper/setup.rb
+++ test/test_helper/setup.rb
@@ -1,9 +1,15 @@
 require "yaml"
+require "logger"
 
 dbconfig = YAML.load_file(path("config/database.yml"))["test"]
+logger = Logger.new path("log/test.log")
+
 ActiveRecord::Base.establish_connection dbconfig
 ActiveRecord::Base.time_zone_aware_attributes = true
 ActiveRecord::Base.default_timezone = :local
+ActiveRecord::Base.logger = logger
+
 DataMapper.setup :default, dbconfig.merge("adapter" => "mysql")
+DataMapper.logger = logger
 
 CachedRecord.setup
