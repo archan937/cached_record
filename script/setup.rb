bc5fb5acd672449ba68780eccb0aa426912166e9
Paul Engel <paul.engel@holder.nl>
2013-12-12 23:57:38 +0100

    Tweaked script/setup.rb a bit

--- script/setup.rb
+++ script/setup.rb
@@ -17,21 +17,21 @@ class Article < ActiveRecord::Base
   belongs_to :author, :class_name => "User"
   has_many :comments
   has_and_belongs_to_many :tags
-  as_memoized_cache :redis, :only => [:title], :include => [:author, :comments, :tags]
+  as_memoized_cache :only => [:title], :include => [:author, :comments, :tags]
 end
 
 class User < ActiveRecord::Base
   has_one :foo, :class_name => "Article", :foreign_key => "foo_id"
-  as_memoized_cache :redis, :only => [:name], :include => [:foo]
+  as_memoized_cache :only => [:name], :include => [:foo]
 end
 
 class Comment < ActiveRecord::Base
   belongs_to :article
   belongs_to :poster, :class_name => "User"
-  as_memoized_cache :redis, :only => [:content], :include => [:poster]
+  as_memoized_cache :only => [:content], :include => [:poster]
 end
 
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :articles
-  as_memoized_cache :redis, :only => [:name]
+  as_memoized_cache :only => [:name]
 end
