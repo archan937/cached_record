2fe0d5632a5a2d17a09e4ad5f00ebf56bcc6de28
Paul Engel <paul.engel@holder.nl>
2013-12-13 02:13:54 +0100

    Being able to specify TTL when invoking Redis#set

--- lib/gem_ext/redis.rb
+++ lib/gem_ext/redis.rb
@@ -1,5 +1,25 @@
+begin
+  require "redis"
+rescue LoadError
+end
+
 class Redis
+
+  alias :set_without_cached_record :set
+  def set(key, value, ttl_or_options = nil)
+    if ttl_or_options.is_a? Integer
+      ttl = ttl_or_options
+      options = {}
+    else
+      options = ttl_or_options || {}
+    end
+    set_without_cached_record(key, value, options).tap do
+      expire key, ttl if ttl
+    end
+  end
+
   def delete(*args)
     del *args
   end
+
 end
--- test/gem_ext/test_redis.rb
+++ test/gem_ext/test_redis.rb
@@ -4,6 +4,24 @@ module GemExt
   class TestRedis < MiniTest::Unit::TestCase
 
     describe Redis do
+      describe "#set" do
+        describe "when passing TTL" do
+          it "invokes set, followed with expire" do
+            redis = Redis.new
+            redis.expects(:set_without_cached_record).with(:foo, :bar, {})
+            redis.expects(:expire).with(:foo, 10)
+            redis.set :foo, :bar, 10
+          end
+        end
+        describe "when passing options hash" do
+          it "invokes set" do
+            redis = Redis.new
+            redis.expects(:set_without_cached_record).with(:foo, :bar, {:ex => 5})
+            redis.set :foo, :bar, {:ex => 5}
+          end
+        end
+      end
+
       describe "#delete" do
         it "calls del" do
           redis = Redis.new
