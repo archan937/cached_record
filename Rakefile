5a41221911688c4c04bbd6a9c2d891ae67e3233d
Paul Engel <paul.engel@holder.nl>
2013-12-20 01:46:02 +0100

    Added rake:benchmark

--- Rakefile
+++ Rakefile
@@ -8,6 +8,57 @@ Rake::TestTask.new do |test|
   test.pattern = "test/**/test_*.rb"
 end
 
+task :benchmark do
+  require_relative "benchmark/setup"
+
+  def benchmark(description, count, interval)
+    Redis.new.flushdb
+    puts "Benchmarking #{description} (#{count} times)"
+    puts "-> [#{"." * 100}] 0.0% in 0.0s"
+    t = Time.now
+    count.times do |i|
+      if i % interval == (interval - 1)
+        print "\e[A\e[K"
+        percentage = (i / count.to_f) * 100
+        puts "-> [#{("+" * percentage.ceil).ljust(100, ".")}] #{"%.1f" % percentage}% in #{"%.2f" % (Time.now - t)}s"
+      end
+      yield
+    end
+  end
+
+  count = 5000
+  benchmark "uncached instances", count, 10 do
+    article = Cached::Article.find 1
+    article.author.foo
+    article.comments[0].poster.foo
+    article.comments[1].poster.foo
+  end
+  benchmark "cached instances", count, 10 do
+    article = Cached::Article.cached 1
+    article.author.foo
+    article.comments[0].poster.foo
+    article.comments[1].poster.foo
+  end
+  benchmark "memoized instances", count, 50 do
+    article = Memoized::Article.cached 1
+    article.author.foo
+    article.comments[0].poster.foo
+    article.comments[1].poster.foo
+  end
+  benchmark "retained instances", count, 50 do
+    article = Retained::Article.cached 1
+    article.author.foo
+    article.comments[0].poster.foo
+    article.comments[1].poster.foo
+  end
+
+  count = 150000
+  benchmark "memoized instances", count, 50  do Memoized::Article.cached 1 end
+  benchmark "retained instances", count, 100 do Retained::Article.cached 1 end
+
+  puts "Done."
+end
+
 namespace :db do
   task :install do
     require "bundler"
---
+++ benchmark/setup.rb
@@ -0,0 +1,76 @@
+require "bundler"
+Bundler.require :default, :development
+require "active_record"
+
+dbconfig = YAML.load_file(File.expand_path("../../config/database.yml", __FILE__))["development"]
+ActiveRecord::Base.establish_connection dbconfig
+ActiveRecord::Base.time_zone_aware_attributes = true
+ActiveRecord::Base.default_timezone = :local
+
+CachedRecord.setup :redis
+
+module Cached
+  class Article < ActiveRecord::Base
+    self.table_name = "articles"
+    belongs_to :author, :class_name => "Cached::User", :foreign_key => "author_id"
+    has_many :comments, :class_name => "Cached::Comment", :foreign_key => "article_id"
+    as_cache :only => [:title], :include => [:author, :comments]
+  end
+
+  class User < ActiveRecord::Base
+    self.table_name = "users"
+    has_one :foo, :class_name => "Cached::Article", :foreign_key => "foo_id"
+    as_cache :only => [:name], :include => [:foo]
+  end
+
+  class Comment < ActiveRecord::Base
+    self.table_name = "comments"
+    belongs_to :article, :class_name => "Cached::Article", :foreign_key => "article_id"
+    belongs_to :poster, :class_name => "Cached::User", :foreign_key => "poster_id"
+    as_cache :only => [:content], :include => [:poster]
+  end
+end
+
+module Memoized
+  class Article < ActiveRecord::Base
+    self.table_name = "articles"
+    belongs_to :author, :class_name => "Memoized::User", :foreign_key => "author_id"
+    has_many :comments, :class_name => "Memoized::Comment", :foreign_key => "article_id"
+    as_memoized_cache :only => [:title], :include => [:author, :comments]
+  end
+
+  class User < ActiveRecord::Base
+    self.table_name = "users"
+    has_one :foo, :class_name => "Memoized::Article", :foreign_key => "foo_id"
+    as_memoized_cache :only => [:name], :include => [:foo]
+  end
+
+  class Comment < ActiveRecord::Base
+    self.table_name = "comments"
+    belongs_to :article, :class_name => "Memoized::Article", :foreign_key => "article_id"
+    belongs_to :poster, :class_name => "Memoized::User", :foreign_key => "poster_id"
+    as_memoized_cache :only => [:content], :include => [:poster]
+  end
+end
+
+module Retained
+  class Article < ActiveRecord::Base
+    self.table_name = "articles"
+    belongs_to :author, :class_name => "Retained::User", :foreign_key => "author_id"
+    has_many :comments, :class_name => "Retained::Comment", :foreign_key => "article_id"
+    as_memoized_cache :only => [:title], :include => [:author, :comments], :retain => 10.seconds
+  end
+
+  class User < ActiveRecord::Base
+    self.table_name = "users"
+    has_one :foo, :class_name => "Retained::Article", :foreign_key => "foo_id"
+    as_memoized_cache :only => [:name], :include => [:foo], :retain => 10.seconds
+  end
+
+  class Comment < ActiveRecord::Base
+    self.table_name = "comments"
+    belongs_to :article, :class_name => "Retained::Article", :foreign_key => "article_id"
+    belongs_to :poster, :class_name => "Retained::User", :foreign_key => "poster_id"
+    as_memoized_cache :only => [:content], :include => [:poster], :retain => 10.seconds
+  end
+end
